5e1a668aa1b63c2738fcb3c1d137171d703a8368
YooDongSeon <inko51366@naver.com>
2023-01-31 23:30:58 +0900

    Refactor: AppConfig에서 역할과 구현 클래스를 분리

--- OOP/AppConfig.swift
+++ OOP/AppConfig.swift
@@ -9,11 +9,32 @@ import Foundation
 
 class AppConfig {
     
+//    func memberService() -> MemberService {
+//        return MemberServiceImpl(MemoryMemberRepository())
+//    }
+//
+//    func orderService() -> OrderService {
+//        return OrderServiceImpl(MemoryMemberRepository(), RateDiscountPolicy())
+//    }
+    
+    
+    
+    // 역할과 구현 클래스가 한눈에 들어온다..
+    // 구현체(impl)를 바라보지말고 프로토콜(인터페이스)을 바라봐라
+    
+    func memberRepository() -> MemberRepository {
+        return MemoryMemberRepository()
+    }
+    
+    func discountPolicy() -> DiscountPolicy {
+        return FixDiscoundPolicy()
+    }
+    
     func memberService() -> MemberService {
-        return MemberServiceImpl(MemoryMemberRepository())
+        return MemberServiceImpl(memberRepository())
     }
     
     func orderService() -> OrderService {
-        return OrderServiceImpl(MemoryMemberRepository(), RateDiscountPolicy())
+        return OrderServiceImpl(memberRepository(), discountPolicy())
     }
 }
