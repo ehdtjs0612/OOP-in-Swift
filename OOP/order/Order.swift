16810e2d8276a0e4720f0ccade7fbb8ede52f798
YooDongSeon <inko51366@naver.com>
2023-01-31 17:55:56 +0900

    Feat: Order 프로토콜과 구현체 생성

---
+++ OOP/order/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  OOP
+//
+//  Created by 유동선 on 2023/01/31.
+//
+
+import Foundation
---
+++ OOP/order/OrderApp.swift
@@ -0,0 +1,8 @@
+//
+//  OrderApp.swift
+//  OOP
+//
+//  Created by 유동선 on 2023/01/31.
+//
+
+import Foundation
