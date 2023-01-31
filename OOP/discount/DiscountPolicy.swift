ec548b11c9cd6e032debb60673a8e523339e6ba7
YooDongSeon <inko51366@naver.com>
2023-01-31 15:42:25 +0900

    할인 정책 프로토콜 생성

---
+++ OOP/discount/DiscountPolicy.swift
@@ -0,0 +1,13 @@
+//
+//  DiscountPolicy.swift
+//  OOP
+//
+//  Created by 유동선 on 2023/01/31.
+//
+
+import Foundation
+
+protocol DiscountPolicy {
+    
+    func discount(member: Member, price: Int) -> Int
+}
